3f934a7b8386093b6d43ba3f6d5d17617d661624
MohithMohan0708 <mohithmohan0708@gmail.com>
2024-06-14 23:01:25 +0530

    feat: add testimonals component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import { footer } from "./footer";
 import { whyChooseUs } from "./whyChooseUs";
 import { services } from "./services";
 import { contactUs } from "./contactUs";
+import { testimonials } from "./testimonals";
 import "./styles.css";
 
 document.body.appendChild(whyChooseUs());
 document.body.appendChild(services());
+document.body.appendChild(testimonials());
 document.body.appendChild(contactUs());
 document.body.appendChild(footer());
+
